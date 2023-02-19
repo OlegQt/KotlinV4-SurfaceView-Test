c5d1eab8c046514ef0910711bcc79f45accf7893
OlegQt <xqjh@mail.ru>
2023-02-19 22:28:13 +0300

    TxtInfo connected via findViewById fun

--- app/src/main/java/App/kotlinv4/MainActivity.kt
+++ app/src/main/java/App/kotlinv4/MainActivity.kt
@@ -2,10 +2,19 @@ package App.kotlinv4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var txt:TextView?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txt=findViewById(R.id.txtInfo)
+        val strInfo:String = "New Application"
+        txt?.text=strInfo
+
+
     }
 }
