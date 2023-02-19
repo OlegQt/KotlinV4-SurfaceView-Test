54be3fc4079f5cead8111bb67f2fd2ac6c51e27a
OlegQt <xqjh@mail.ru>
2023-02-19 22:46:55 +0300

    Trying to add Derived surfView to xml

--- app/src/main/java/App/kotlinv4/MainActivity.kt
+++ app/src/main/java/App/kotlinv4/MainActivity.kt
@@ -10,9 +10,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        setContentView(Screen(this))
+        //setContentView(Screen(this))
 
         txt=findViewById(R.id.txtInfo)
         val strInfo:String = "New Application"
